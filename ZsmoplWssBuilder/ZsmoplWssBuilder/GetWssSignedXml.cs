a4aad34aa90becac973ad4dc60de875a34d27e80
Jacek Kashiash Kosiński <kashiash@gmail.com>
2019-03-14 13:16:34 +0100

    dodalem dll z marshalingiem

---
+++ ZsmoplWssBuilder/ZsmoplWssBuilder/GetWssSignedXml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RGiesecke.DllExport;
+using System.Runtime.InteropServices;
+
+namespace ZsmoplWssBuilder
+{
+    public static class WssSignedXml
+    {
+        [DllExport(nameof(GetWssSignedXml), CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public static string GetWssSignedXml([MarshalAs(UnmanagedType.BStr)] string certificate,
+            [MarshalAs(UnmanagedType.BStr)] string certPassword, [MarshalAs(UnmanagedType.BStr)] string bodyXml)
+        {
+            return "";
+        }
+    }
+}
