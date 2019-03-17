f4116e5a5ff5a6a25bceca0f4ef36cb46acde2e4
Jacek Kosiński <kashiash@gmail.com>
2019-03-17 18:17:28 +0100

    .

---
+++ Gabos.Zsmopl.Clarion/WssSignedXml.cs
@@ -0,0 +1,23 @@
+﻿using Gabos.Zsmolp.Client;
+using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gabos.Zsmopl.Clarion
+{
+    public static class WssSignedXml
+    {
+        [DllExport(nameof(GetWssSignedXml), CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public static string GetWssSignedXml([MarshalAs(UnmanagedType.BStr)] string certificate,
+            [MarshalAs(UnmanagedType.BStr)] string certPassword, [MarshalAs(UnmanagedType.BStr)] string bodyXml)
+        {
+            var res = ZsmoplFactory.GetSignedRequest(certificate, certPassword, bodyXml);
+            return res;
+        }
+    }
+}
