79658a0da06012061c0d90fea7caf656ad830bf2
kashiash <kashiash@gmail.com>
2019-03-20 12:51:55 +0100

    dodalem obsluge full body

--- Gabos.Zsmolp.Client/ZsmoplFactory.cs
+++ Gabos.Zsmolp.Client/ZsmoplFactory.cs
@@ -14,6 +14,11 @@ namespace Gabos.Zsmolp.Client
             UnlockChilkat();
             return SignXml(certificate, password, body);
         }
+        public static string GetSignedRequestFullBody(string certificate, string password, string body)
+        {
+            UnlockChilkat();
+            return SignXmlFull(certificate, password, body);
+        }
         private static void UnlockChilkat()
         {
             Chilkat.Global glob = new Chilkat.Global();
@@ -79,7 +84,7 @@ namespace Gabos.Zsmolp.Client
             success = cert.LoadPfxFile(certWss, passWss);
             if (success != true)
                 Debug.WriteLine(cert.LastErrorText);
-                //  return;
+            //  return;
 
             gen.SetX509Cert(cert, true);
 
@@ -112,7 +117,7 @@ namespace Gabos.Zsmolp.Client
             success = gen.CreateXmlDSigSb(sbXml);
             if (success != true)
                 Debug.WriteLine(gen.LastErrorText);
-                // return;
+            // return;
 
             // -----------------------------------------------
 
@@ -157,5 +162,110 @@ namespace Gabos.Zsmolp.Client
             Debug.WriteLine("All signatures were successfully verified.");
             return success;
         }
+
+
+
+        #region podpis pelnego body
+        public static string SignXmlFull(string certWss, string passWss, string body)
+        {
+            bool success = true;
+            // Create the above XML to be signed...
+            Chilkat.Xml xmlToSign = new Chilkat.Xml();
+            xmlToSign.Tag = "soapenv:Envelope";
+            xmlToSign.AddAttribute("xmlns:obs", "http://csioz.gov.pl/zsmopl/ws/obslugakomunikatow/");
+            xmlToSign.AddAttribute("xmlns:soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+            xmlToSign.UpdateAttrAt("soapenv:Header|wsse:Security", true, "xmlns:wsse", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+            xmlToSign.UpdateAttrAt("soapenv:Header|wsse:Security", true, "xmlns:wsu", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+            xmlToSign.UpdateAttrAt("soapenv:Header|wsse:Security", true, "soapenv:mustUnderstand", "1");
+            xmlToSign.UpdateAttrAt("soapenv:Header|wsse:Security|wsse:BinarySecurityToken", true, "EncodingType", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary");
+            xmlToSign.UpdateAttrAt("soapenv:Header|wsse:Security|wsse:BinarySecurityToken", true, "ValueType", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509PKIPathv1");
+            xmlToSign.UpdateAttrAt("soapenv:Header|wsse:Security|wsse:BinarySecurityToken", true, "wsu:Id", "X509-02BF0107214FC61449FD0013DF68F0359");
+            // Note: The content of this XML node is a placeholder that will be updated below with the X509PKIPathv1 for the signing certificate.
+            xmlToSign.UpdateChildContent("soapenv:Header|wsse:Security|wsse:BinarySecurityToken", "BinarySecurityToken_Base64Binary_Content");
+            xmlToSign.UpdateAttrAt("soapenv:Body", true, "xmlns:wsu", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+            xmlToSign.UpdateAttrAt("soapenv:Body", true, "wsu:Id", "id-396BB6026342EB5C0E1EA73593B3CC098");
+            xmlToSign.UpdateChildContent("soapenv:Body", "Body_Content");
+
+
+            Chilkat.XmlDSigGen gen = new Chilkat.XmlDSigGen();
+
+            gen.SigLocation = "soapenv:Envelope|soapenv:Header|wsse:Security";
+            gen.SigLocationMod = 0;
+            gen.SigId = "SIG-BB965DFC3C8AAF87903C0ED898B8D2A8D";
+            gen.SigNamespacePrefix = "ds";
+            gen.SigNamespaceUri = "http://www.w3.org/2000/09/xmldsig#";
+            gen.SignedInfoCanonAlg = "EXCL_C14N";
+            gen.SignedInfoDigestMethod = "sha1";
+
+
+            // Set the KeyInfoId before adding references..
+            gen.KeyInfoId = "KI-9D95C38916099AD2EE87DDAC1A76E97E4";
+
+            // -------- Reference 1 --------
+            gen.AddSameDocRef("id-396BB6026342EB5C0E1EA73593B3CC098", "sha1", "EXCL_C14N", "obs", "");
+
+            // The reference to be produced in the Signature should look like this:
+
+            // <ds:Reference URI="#id-396BB6026342EB5C0E1EA73593B3CC098">
+            //     <ds:Transforms><ds:Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#">
+            //         <ec:InclusiveNamespaces xmlns:ec="http://www.w3.org/2001/10/xml-exc-c14n#" PrefixList="obs"></ec:InclusiveNamespaces>
+            //     </ds:Transform></ds:Transforms>
+            //     <ds:DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"></ds:DigestMethod>
+            //     <ds:DigestValue>2e9hZYj/CN2nPsgQqUraU43k3ds=</ds:DigestValue>
+            // </ds:Reference>
+            // 
+
+            // Provide a certificate + private key. (PFX password is test123)
+            Chilkat.Cert cert = new Chilkat.Cert();
+            success = cert.LoadPfxFile(certWss, passWss);
+            if (success != true)
+                Debug.WriteLine(cert.LastErrorText);
+            //  return;
+
+            gen.SetX509Cert(cert, true);
+
+            gen.KeyInfoType = "Custom";
+
+            // Create the custom KeyInfo XML..
+            Chilkat.Xml xmlCustomKeyInfo = new Chilkat.Xml();
+            xmlCustomKeyInfo.Tag = "wsse:SecurityTokenReference";
+            xmlCustomKeyInfo.AddAttribute("wsse11:TokenType", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509PKIPathv1");
+            xmlCustomKeyInfo.AddAttribute("xmlns:wsse11", "http://docs.oasis-open.org/wss/oasis-wss-wssecurity-secext-1.1.xsd");
+            xmlCustomKeyInfo.AddAttribute("wsu:Id", "STR-FF238E7C061332C5B19752C2FBC8CDEF2");
+            xmlCustomKeyInfo.UpdateAttrAt("wsse:Reference", true, "URI", "#X509-02BF0107214FC61449FD0013DF68F0359");
+            xmlCustomKeyInfo.UpdateAttrAt("wsse:Reference", true, "ValueType", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509PKIPathv1");
+
+            xmlCustomKeyInfo.EmitXmlDecl = false;
+            gen.CustomKeyInfoXml = xmlCustomKeyInfo.GetXml();
+
+            // Load XML to be signed...
+            Chilkat.StringBuilder sbXml = new Chilkat.StringBuilder();
+            xmlToSign.GetXmlSb(sbXml);
+
+            // Update BinarySecurityToken_Base64Binary_Content with the actual X509PKIPathv1 of the signing cert.
+            int nReplaced = sbXml.Replace("BinarySecurityToken_Base64Binary_Content", cert.X509PKIPathv1());
+
+            int nReplacedBody = sbXml.Replace("Body_Content", body);
+
+            gen.Behaviors = "IndentedSignature";
+
+            // Sign the XML...
+            success = gen.CreateXmlDSigSb(sbXml);
+            if (success != true)
+                Debug.WriteLine(gen.LastErrorText);
+            // return;
+
+            // -----------------------------------------------
+
+            // Save the signed XML to a file.
+            //  success = sbXml.WriteFile(@"c:\apps\SignedRequest.xml", "utf-8", false);
+
+            Debug.WriteLine(sbXml.GetAsString());
+
+            success = VerifySignature(sbXml);
+
+            return sbXml.ToString();
+        }
+        #endregion
     }
 }
--- ZsmoplWssBuilder/ZsmoplWssBuilder/GetWssSignedXml.cs
+++ ZsmoplWssBuilder/ZsmoplWssBuilder/GetWssSignedXml.cs
@@ -9,8 +9,8 @@ namespace ZsmoplWssBuilder
         [DllExport(nameof(GetWssSignedXmlX), CallingConvention = CallingConvention.StdCall)]
         [return: MarshalAs(UnmanagedType.BStr)]
         public static string GetWssSignedXmlX([MarshalAs(UnmanagedType.BStr)] string certificate,
-    [MarshalAs(UnmanagedType.BStr)]  string certPassword,
-    [MarshalAs(UnmanagedType.BStr)]  string bodyXml)
+            [MarshalAs(UnmanagedType.BStr)]  string certPassword,
+            [MarshalAs(UnmanagedType.BStr)]  string bodyXml)
         {
             var Odp = ZsmoplFactory.GetSignedRequest(certificate, certPassword, bodyXml);
 
@@ -20,11 +20,23 @@ namespace ZsmoplWssBuilder
         [DllExport(nameof(GetWssSignedXml), CallingConvention = CallingConvention.StdCall)]
         [return: MarshalAs(UnmanagedType.BStr)]
         public static string GetWssSignedXml([MarshalAs(UnmanagedType.BStr)] string certificate,
-        [MarshalAs(UnmanagedType.BStr)] string certPassword,
-        [MarshalAs(UnmanagedType.BStr)] string bodyXml
+            [MarshalAs(UnmanagedType.BStr)] string certPassword,
+            [MarshalAs(UnmanagedType.BStr)] string bodyXml
             )
         {
             var Odp = ZsmoplFactory.GetSignedRequest(certificate, certPassword, bodyXml);
+
+            return Odp;
+        }
+
+        [DllExport(nameof(GetWssSignedFullXml), CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public static string GetWssSignedFullXml([MarshalAs(UnmanagedType.BStr)] string certificate,
+            [MarshalAs(UnmanagedType.BStr)] string certPassword,
+            [MarshalAs(UnmanagedType.BStr)] string bodyXml
+    )
+        {
+            var Odp = ZsmoplFactory.GetSignedRequestFullBody(certificate, certPassword, bodyXml);
 
             return Odp;
         }
