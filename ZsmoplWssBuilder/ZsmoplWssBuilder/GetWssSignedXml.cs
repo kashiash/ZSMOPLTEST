59aea7eba1396460cfcfb3357afb8fafdd4fa946
Jacek Kashiash Kosiński <kashiash@gmail.com>
2019-03-21 09:46:01 +0100

    .

--- ZsmoplWssBuilder/ZsmoplWssBuilder/GetWssSignedXml.cs
+++ ZsmoplWssBuilder/ZsmoplWssBuilder/GetWssSignedXml.cs
@@ -10,8 +10,7 @@ namespace ZsmoplWssBuilder
         [return: MarshalAs(UnmanagedType.BStr)]
         public static string GetWssSignedXmlX([MarshalAs(UnmanagedType.BStr)] string certificate,
             [MarshalAs(UnmanagedType.BStr)]  string certPassword,
-            [MarshalAs(UnmanagedType.BStr)]  string bodyXml)
-        {
+            [MarshalAs(UnmanagedType.BStr)]  string bodyXml){
             var Odp = ZsmoplFactory.GetSignedRequest(certificate, certPassword, bodyXml);
 
             return Odp;
@@ -21,8 +20,7 @@ namespace ZsmoplWssBuilder
         [return: MarshalAs(UnmanagedType.BStr)]
         public static string GetWssSignedXml([MarshalAs(UnmanagedType.BStr)] string certificate,
             [MarshalAs(UnmanagedType.BStr)] string certPassword,
-            [MarshalAs(UnmanagedType.BStr)] string bodyXml)
-        {
+            [MarshalAs(UnmanagedType.BStr)] string bodyXml){
             var Odp = ZsmoplFactory.GetSignedRequest(certificate, certPassword, bodyXml);
 
             return Odp;
@@ -32,8 +30,7 @@ namespace ZsmoplWssBuilder
         [return: MarshalAs(UnmanagedType.BStr)]
         public static string GetWssSignedStatusXml([MarshalAs(UnmanagedType.BStr)] string certificate,
             [MarshalAs(UnmanagedType.BStr)] string certPassword,
-            [MarshalAs(UnmanagedType.BStr)] string ID)
-        {
+            [MarshalAs(UnmanagedType.BStr)] string ID){
             var Odp = ZsmoplFactory.GetSignedStatusRequest(certificate, certPassword, ID);
 
             return Odp;
@@ -45,8 +42,7 @@ namespace ZsmoplWssBuilder
             [MarshalAs(UnmanagedType.BStr)] string certPassword,
             [MarshalAs(UnmanagedType.BStr)] string bodyXml,
             [MarshalAs(UnmanagedType.BStr)] string bodyPrefix,
-            [MarshalAs(UnmanagedType.BStr)] string bodyNameSpace)
-        {
+            [MarshalAs(UnmanagedType.BStr)] string bodyNameSpace){
             var Odp = ZsmoplFactory.GetSignedRequestFullBody(certificate, certPassword, bodyXml, bodyPrefix, bodyNameSpace);
 
             return Odp;
@@ -56,8 +52,7 @@ namespace ZsmoplWssBuilder
         [return: MarshalAs(UnmanagedType.BStr)]
         public static string WyslijKomunikatOS([MarshalAs(UnmanagedType.BStr)] string certificate,
             [MarshalAs(UnmanagedType.BStr)] string certPassword,
-            [MarshalAs(UnmanagedType.BStr)] string bodyXml)
-        {
+            [MarshalAs(UnmanagedType.BStr)] string bodyXml){
             var Odp = ZsmoplFactory.WyslijOS(certificate, certPassword, bodyXml);
 
             return Odp;
@@ -67,8 +62,7 @@ namespace ZsmoplWssBuilder
         [return: MarshalAs(UnmanagedType.BStr)]
         public static string ZapytajOStatusKomunikatu([MarshalAs(UnmanagedType.BStr)] string certificate,
             [MarshalAs(UnmanagedType.BStr)] string certPassword,
-            [MarshalAs(UnmanagedType.BStr)] string idKomunikatu)
-        {
+            [MarshalAs(UnmanagedType.BStr)] string idKomunikatu){
             var Odp = ZsmoplFactory.PobierzStatus(certificate, certPassword, idKomunikatu);
 
             return Odp;
